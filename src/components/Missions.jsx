05079741c9b1fc519ec2f654741d9a54e914fd7c
danricardo88 <daniel.ricardo88@outlook.com>
2022-07-21 00:07:02 -0300

    RECEBA DEUS-TUDO-OK

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import Title from './Title';
+import missions from '../data/missions';
+import MissionsCard from './MissionCard';
 
 class Missions extends Component {
   render() {
     return (
-      <div data-testid="missions"> </div>
+      <div data-testid="missions">
+        <Title headline="Missões" />
+        {missions.map((item, key) => (<MissionsCard
+          key={ key }
+          name={ item.name }
+          year={ item.year }
+          country={ item.country }
+          destination={ item.destination }
+        />
+        ))}
+      </div>
     );
   }
 }
